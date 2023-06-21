5843cc1911f328e18f9fce5a58e153bac6971eea
BrandonLee283 <reyesmorenobrandonlee@gmail.com>
2023-06-20 23:41:34 -0600

    regradando css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nosotros from './Components/Nosotros';
 import Productos from './Components/Productos';
 import Contacto from './Components/Contacto';
+import Home from './Components/Home';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home></Home>} />
           <Route path="/nosotros" element={<Nosotros></Nosotros>} />
           <Route path="/productos" element={<Productos></Productos>} />
           <Route path="/contacto" element={<Contacto></Contacto>} />
