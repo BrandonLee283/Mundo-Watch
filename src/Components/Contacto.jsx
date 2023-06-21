e47401917f5e32497de5e762f65fee78314f21fd
Angel <angellow080801@gmail.com>
2023-06-21 01:13:56 -0600

    cambios de contacto correos

--- src/Components/Contacto.jsx
+++ src/Components/Contacto.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import '../Styles/style.css'
-import { useNavigate, useRef } from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 import emailjs from '@emailjs/browser';
 
