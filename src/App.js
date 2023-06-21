1f522df92c405863c3cbfbc2b60718dc35f3234f
BrandonLee283 <reyesmorenobrandonlee@gmail.com>
2023-06-20 23:39:39 -0600

    regradando app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nosotros from './Components/Nosotros';
+import Productos from './Components/Productos';
+import Contacto from './Components/Contacto';
 function App() {
   return (
     <div className="App">
-      <p>Hola</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/nosotros" element={<Nosotros></Nosotros>} />
+          <Route path="/productos" element={<Productos></Productos>} />
+          <Route path="/contacto" element={<Contacto></Contacto>} />
+          <Route path="*" element={<p>No encontrado</p>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
